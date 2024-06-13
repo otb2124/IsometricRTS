3e5488859e6b7080dc518221f1c90f406d9963d0
otb2124 <otb2124@g.rit.edu>
2024-06-13 17:18:48 +0200

    Create GameManager.cs

---
+++ GameManager.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace IsometricRTS
+{
+    public class GameManager
+    {
+
+        private readonly Map map = new Map();
+        private Player player;
+
+        public void init()
+        {
+            player = new Player(Globals.LoadTexture("Content/player/player0.png"), Vector2.Zero);
+            Globals.Player = player;
+        }
+
+        public void Update()
+        {
+            InputManager.Update();
+            map.Update();
+        }
+
+
+
+        public void Draw()
+        {
+            map.Draw();
+            player.Draw();
+        }
+    }
+}
