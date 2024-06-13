bf942cbbbb9068252402e990d8f8fe07ea68390b
otb2124 <otb2124@g.rit.edu>
2024-06-13 17:18:47 +0200

    Create Game1.cs

---
+++ Game1.cs
@@ -0,0 +1,55 @@
+﻿using IsometricRTS;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+
+namespace IsometricRTS
+{
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        GameManager manager;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            Globals.Content = Content;
+            Globals.GraphicsDeviceManager = graphics;
+            IsMouseVisible = true;
+
+
+           
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            Globals.SpriteBatch = spriteBatch;
+
+
+            manager = new GameManager();
+            manager.init();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            Globals.Update(gameTime);
+            manager.Update();
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.White);
+
+            Globals.SpriteBatch.Begin();
+            manager.Draw();
+            Globals.SpriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
