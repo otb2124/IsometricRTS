d0e514d9b74cd3e8a8b0598ead8113c370e51ac3
otb2124 <otb2124@g.rit.edu>
2024-06-13 17:19:30 +0200

    Create Player.cs

---
+++ Player.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace IsometricRTS
+{
+    public class Player
+    {
+        private Texture2D _texture;
+        private Vector2 _position;
+
+        public Player(Texture2D texture, Vector2 startPosition)
+        {
+            _texture = texture;
+            _position = startPosition;
+        }
+
+        public void MoveTo(Vector2 newPosition)
+        {
+            _position = newPosition;
+        }
+
+        public void Draw()
+        {
+            Vector2 scale = new Vector2(Globals.GameScale, Globals.GameScale);
+            Globals.SpriteBatch.Draw(_texture, _position, null, Color.White, 0f, Vector2.Zero, scale, SpriteEffects.None, 0f);
+        }
+    }
+}
