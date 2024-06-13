63fb8796bfcbce5dbd5419182deffd25bcd61bee
otb2124 <otb2124@g.rit.edu>
2024-06-13 17:18:52 +0200

    Create InputManager.cs

---
+++ InputManager.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System.Diagnostics;
+
+namespace IsometricRTS
+{
+    public static class InputManager
+    {
+        private static Point _direction;
+        public static Point Direction => _direction;
+
+        private static MouseState _lastMouseState;
+        public static Point MousePosition => Mouse.GetState().Position;
+
+        public static bool IsLeftMouseClick()
+        {
+            var mouseState = Mouse.GetState();
+            bool isClick = mouseState.LeftButton == ButtonState.Pressed;
+            _lastMouseState = mouseState;
+            return isClick;
+        }
+        public static void Update()
+        {
+            _lastMouseState = Mouse.GetState();
+        }
+    }
+}
